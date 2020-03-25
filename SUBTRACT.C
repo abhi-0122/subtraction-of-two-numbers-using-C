19801efaf77ad7b4bfaa5c0ad9a6e7a347955c39
Kumar Abhishek <59106601+abhi-0122@users.noreply.github.com>
2020-03-25 11:31:37 +0530

    Add files via upload

---
+++ SUBTRACT.C
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int a,b,sub;
+ printf("Enter first number");
+ scanf("%d",&a);
+ printf("Enter second number");
+ scanf("%d",&b);
+ sub=a-b;
+ printf("The subtraction of numbers %d and %d is %d";a,b,sub);
+getch();
+}
